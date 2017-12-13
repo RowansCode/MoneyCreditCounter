886636efbb6ce284a55f426e23d314900e0a7f85
Rowan <b7012067@my.shu.ac.uk>
2017-12-13 18:00:12 +0000

    started some code

--- MoneyCreditCount/MoneyCreditCount/Form1.cs
+++ MoneyCreditCount/MoneyCreditCount/Form1.cs
@@ -12,12 +12,47 @@ namespace MoneyCreditCount
 {
     public partial class MoneyCreditCounter : Form
     {
+        int costPerCredit = 5;
+        int totalValue = 0;
+
+        
+
         public MoneyCreditCounter()
         {
             InitializeComponent();
+
+            //textBox1.Text = totalValue.ToString();
         }
 
         private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoneyCreditCounter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (costPerCredit <= 0)
+            {
+                MessageBox.Show("Please enter the cost per credit");
+            }
+            else
+            {
+                totalValue = totalValue + 1;
+                textBox1.Text = totalValue.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
